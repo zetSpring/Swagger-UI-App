6df79a4458c3d0649d2f96e11862275c5e84f24d
zukoyawa <czyawa@gmail.com>
2019-07-12 15:15:44 +0200

    Initial commit: project creation for Swagger documentation udemy course

---
+++ src/main/java/com/zuko/swagger/SwaggerUiAppApplication.java
@@ -0,0 +1,13 @@
+package com.zuko.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerUiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerUiAppApplication.class, args);
+	}
+
+}
